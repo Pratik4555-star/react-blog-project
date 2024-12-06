a0edaefb8cff6cd84d5c903c66f78bfd023be88d
Pratik4555-star <pratikmahadik554@gmail.com>
2024-12-06 14:40:13 +0530

    adding the vlog project files to git

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import conf from "./conf/conf"
+
+
+
+function App() {
+  console.log(conf.appwriteUrl)
+  return (
+    <>
+    <h1 className="text-7xl text-slate-800">Hello project</h1>
+    </>
+  )
+}
+
+export default App
