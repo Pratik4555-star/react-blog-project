91ac2d05da60d6a2e4d212bd3e9e05699ca932b0
Pratik4555-star <pratikmahadik554@gmail.com>
2024-12-07 10:26:54 +0530

    `feat: updated App component to use React hooks and Redux for authentication`

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,40 @@
-import conf from "./conf/conf"
+import { useState } from "react"
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import authService from './appwrite/auth'
+import {login,logout} from'./store/authSlice'
+import { Footer, Header } from "./components";
+// import { Outlet } from "react-router-dom";
 
 
 
 function App() {
-  console.log(conf.appwriteUrl)
-  return (
-    <>
-    <h1 className="text-7xl text-slate-800">Hello project</h1>
-    </>
-  )
+  const [loading, setloading] = useState(true);
+  const dispatch = useDispatch()
+
+useEffect(()=>{
+  authService.getCurrentUser()
+  .then((userdata)=>{
+    if(userdata){
+      dispatch(login({userdata}))
+    }else{
+      dispatch(logout())
+    }
+  })
+  .finally(()=> setloading(false))
+},[]) 
+
+return !loading ? (
+  <div className=" min-h-screen text-white bg-gray-800 flex flex-wrap content-between">
+    <div className="block w-full">
+      <Header/>
+      <main>
+        {/* <Outlet/> */}todo
+      </main>
+      <Footer/>
+    </div>
+  </div>
+):(null)
 }
 
 export default App
